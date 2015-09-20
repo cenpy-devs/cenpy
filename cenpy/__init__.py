4f2f486934203ee979db42cfdef3484a37c2aab3
Levi Wolf <ljw2@asu.edu>
2015-09-20 14:35:15 -0700

    add version checks, fix path, and version attribute

--- cenpy/__init__.py
+++ cenpy/__init__.py
@@ -1,8 +1,8 @@
 __author__ = 'Levi John Wolf ljw2@asu.edu'
-
+#__version__ == '0.8.8.dev'
 
 import cenpy.explorer
 import cenpy.base
-
+from cenpy._version import version as _version
 
 #__all__ = ['explorer', 'base']
