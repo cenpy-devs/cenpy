7bc2390f18baf32e003bb1063e2d073ccf9960a3
Levi Wolf <ljw2@asu.edu>
2015-09-20 15:09:08 -0700

    fix errors in tests

--- cenpy/tests/test_remote.py
+++ cenpy/tests/test_remote.py
@@ -3,4 +3,7 @@ import cenpy
 
 class test_Remote(unittest.TestCase):
     def test_connection(self):
-        cenpy.base.Connection(cenpy.base.available()[0])
+        cenpy.base.Connection(cenpy.explorer.available()[0])
+
+if __name__ == '__main__':
+    unittest.main()
