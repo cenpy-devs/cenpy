eb112baffb66107ea4366226a5c227e64fe3b293
ljwolf <levi.john.wolf@gmail.com>
2016-05-05 17:51:06 -0700

    prep for release

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='cenpy',
-        version='0.9.1',
+        version='0.9.2',
       description='Explore and download data from Census APIs',
       url='https://github.com/ljwolf/cenpy',
       author='Levi John Wolf',
       author_email='levi.john.wolf@gmail.com',
       license='3-Clause BSD',
       packages=['cenpy'],
-      install_requires=['pandas', 'requests'],
+      install_requires=['pandas', 'requests', 'pysal'],
       package_data={'cenpy': ['stfipstable.csv']},
       zip_safe=False)
