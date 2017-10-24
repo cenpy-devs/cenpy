2f743a7edeb293c6da80c6690290e0320c11c9e3
ljwolf <levi.john.wolf@gmail.com>
2017-10-25 00:06:53 +0100

    import from the package, not from the environment

--- cenpy/__init__.py
+++ cenpy/__init__.py
@@ -1,10 +1,10 @@
 __author__ = 'Levi John Wolf ljw2@asu.edu'
 #__version__ == '0.9.8.dev'
 
-import cenpy.explorer
-import cenpy.base
-from cenpy._version import version as _version
-from cenpy.tools import _load_sitekey
+from . import explorer
+from . import base
+from ._version import version as _version
+from .tools import _load_sitekey
 
 SITEKEY = _load_sitekey()
 
