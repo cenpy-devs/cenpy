b60a4c793f2b6e27b83bfee4df0e48505b3c4c4d
ljwolf <levi.john.wolf@gmail.com>
2018-03-20 20:19:50 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cenpy',
-        version='0.9.6',
+        version='0.9.7',
       description='Explore and download data from Census APIs',
       url='https://github.com/ljwolf/cenpy',
       author='Levi John Wolf',
