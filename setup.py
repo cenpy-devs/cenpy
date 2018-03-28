22a0be3b77ffcb5bdf4a30000888cf6d4d6c78eb
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2018-03-28 09:19:02 -0400

    Fix setup.py to keep install consistent with testing

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='cenpy',
-        version='0.9.7',
+      version='0.9.7',
       description='Explore and download data from Census APIs',
       url='https://github.com/ljwolf/cenpy',
       author='Levi John Wolf',
       author_email='levi.john.wolf@gmail.com',
       license='3-Clause BSD',
-      python_requires='>=3.4',
+      python_requires='>=3.5',
       packages=['cenpy'],
       install_requires=['pandas', 'requests', 'pysal'],
       package_data={'cenpy': ['stfipstable.csv']},
