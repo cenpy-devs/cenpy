22a211a87f72be405ff1d33352b07d6f687aa063
ljwolf <levi.john.wolf@gmail.com>
2019-03-15 08:29:12 +0000

    prep for next version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cenpy',
-      version='0.9.8',
+      version='0.9.9',
       description='Explore and download data from Census APIs',
       url='https://github.com/ljwolf/cenpy',
       author='Levi John Wolf',
