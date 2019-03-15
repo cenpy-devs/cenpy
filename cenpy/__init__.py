725281d9406eebbd3842caa890f9fad86bbd688d
ljwolf <levi.john.wolf@gmail.com>
2019-03-15 09:14:56 +0000

    remove old version

--- cenpy/__init__.py
+++ cenpy/__init__.py
@@ -3,7 +3,6 @@ __author__ = 'Levi John Wolf ljw2@asu.edu'
 
 from . import explorer
 from . import base
-from ._version import version as _version
 from .tools import _load_sitekey
 
 SITEKEY = _load_sitekey()
