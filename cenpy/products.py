6ea6b6b218a1a55f97e51fbb2cdb3f51de28da88
ljwolf <levi.john.wolf@gmail.com>
2019-04-08 17:49:45 +0100

    fix colons endings in the level checks

--- cenpy/products.py
+++ cenpy/products.py
@@ -129,7 +129,7 @@ class _Product(object):
                                        how='inner', op='within')
 
         data = []
-        if level == 'county'
+        if level == 'county':
             grouper = involved.groupby('STATE')
         else:
             grouper = involved.groupby(['STATE','COUNTY']) 
@@ -325,7 +325,7 @@ class ACS(_Product):
                    layername, cache_name,
                    strict_within=True,
                    return_bounds=False, geometry_precision=2):
-        if level not in self._layer_lookup().keys()
+        if level not in self._layer_lookup().keys():
             raise NotImplementedError('Only levels {} are supported. You provided {}.'
                                       'Try picking the state containing that level,'
                                       ' and then selecting from that data after it is'
