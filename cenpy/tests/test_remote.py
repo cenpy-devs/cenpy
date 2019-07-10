95b8b4101c18ea899a03df59a78f5745da39f73a
ljwolf <levi.john.wolf@gmail.com>
2019-07-11 00:19:11 +0100

    keep pulling base out of package

--- cenpy/tests/test_remote.py
+++ cenpy/tests/test_remote.py
@@ -3,7 +3,7 @@ import cenpy
 
 class test_remote(unittest.TestCase):
     def test_connection(self):
-        cenpy.base.Connection(cenpy.explorer.available(verbose=False)[0])
+        cenpy.remote.APIConnection(cenpy.explorer.available(verbose=False)[0])
 
 if __name__ == '__main__':
     unittest.main()
